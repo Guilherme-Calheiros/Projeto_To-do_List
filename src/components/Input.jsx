50383ef5eb4c707be4c71bf16c5cb4ab6f8b16c3
Guilherme-Calheiros <guilhermecalheiros522@gmail.com>
2023-12-12 10:37:34 -0300

    feat: componente para input

---
+++ src/components/Input.jsx
@@ -0,0 +1,11 @@
+const Input = ({ type, placeholder, value, onChange }) => {
+    return <input
+        className="bg-slate-100 p-2 rounded-xl outline-none drop-shadow w-5/6"
+        value={value}
+        onChange={onChange}
+        type={type}
+        placeholder={placeholder}
+    />
+}
+
+export default Input
