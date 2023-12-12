6e5252397db696e64e6c89ae4ef940e3722acfb3
Guilherme-Calheiros <guilhermecalheiros522@gmail.com>
2023-12-12 10:35:44 -0300

    feat: adicionando as cores principais do projeto

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,16 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          100: "#E5F2FF",
+          200: "#3A97F7",
+          300: "#1380F0",
+          400: "#005CB9",
+        },
+      },
+    },
   },
   plugins: [],
 }
