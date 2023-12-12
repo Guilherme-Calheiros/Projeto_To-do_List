5cd213e3002fe8b050a19b6b923a5237bb7bebe6
Guilherme-Calheiros <guilhermecalheiros522@gmail.com>
2023-12-12 10:39:13 -0300

    feat: componente para o card de acesso

---
+++ src/components/CardAcesso.jsx
@@ -0,0 +1,11 @@
+
+const CardAcesso = ({children, text}) => {
+    return (
+        <div className="bg-white rounded-xl flex flex-col w-80 justify-center items-center gap-4 p-4">
+            <h1 className="text-4xl text-primary-300 font-bold mb-8">{text}</h1>
+            {children}
+        </div>
+    )
+}
+
+export default CardAcesso
