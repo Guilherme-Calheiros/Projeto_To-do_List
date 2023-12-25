ff8926e52fc07e5e9ada9378dc0255d146ace643
Guilherme-Calheiros <guilhermecalheiros522@gmail.com>
2023-12-25 16:14:34 -0300

    feat: criando um component para a função de signout

---
+++ src/components/SignoutButton.jsx
@@ -0,0 +1,20 @@
+import Button from "./Button.jsx";
+import { useNavigate } from "react-router-dom";
+import UseAuthContext from "../hooks/UseAuthContext.jsx";
+
+const SignoutButton = () => {
+
+    const { signout } = UseAuthContext()
+    const navigate = useNavigate()
+
+    const handleSignout = () => {
+        signout()
+        navigate("/")
+    }
+
+    return (
+        <Button text="Sair" onClick={handleSignout} />
+    )
+};
+
+export default SignoutButton;
