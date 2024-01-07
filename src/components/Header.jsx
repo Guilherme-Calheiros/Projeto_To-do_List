9031485140fa6fca74e9e4da7656c9cf89c57394
Guilherme-Calheiros <guilhermecalheiros522@gmail.com>
2024-01-07 13:01:39 -0300

    fix: resolvendo o problema de clique no menu hamburguer

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -15,38 +15,31 @@ const Header = ({ username }) => {
             <div className="md:hidden">
                 <Image src={Logo} alt="Imagem da logo To-do" w="150px" />
             </div>
-            <div className=" hidden space-y-1 md:block">
+            <div className="space-y-1">
                 {!isVisible && (
-                    <button className="text-white text-xl font-bold" onClick={() => setIsVisible(true)}>{username}</button>
+                    <>
+                        <button className="text-white text-xl font-bold hidden md:block" onClick={() => setIsVisible(true)}>{username}</button>
+                        <button className="flex flex-col gap-1 md:hidden" onClick={() => setIsVisible(true)} >
+                            <div className="w-6 h-1 bg-white"></div>
+                            <div className="w-6 h-1 bg-white"></div>
+                            <div className="w-6 h-1 bg-white"></div>
+                        </button>
+                    </>
                 )}
 
                 {isVisible && (
-                    <ul className="bg-white w-1/3 pb-10 absolute top-1 right-1 duration-150 flex flex-col space-y-3 justify-end items-center rounded border-black shadow-lg z-10">
+                    <ul className="bg-white w-full md:w-1/3 pb-10 absolute md:top-1 md:right-1 top-0 right-0 duration-150 flex flex-col space-y-3 justify-end items-center rounded border-black shadow-lg z-10">
                         <button className="px-10 py-8 relative ml-auto" onClick={() => setIsVisible(false)}>
                             <div className="w-6 h-1 rotate-45 absolute bg-black"></div>
                             <div className="w-6 h-1 -rotate-45 absolute bg-black"></div>
                         </button>
                         <li className="text-black text-lg font-bold">{username}</li>
                         <SignoutButton />
-                        <Delete/>
+                        <Delete />
                     </ul>
                 )}
             </div>
-            <button className="space-y-1 group md:hidden">
-                <div className="w-6 h-1 bg-white"></div>
-                <div className="w-6 h-1 bg-white"></div>
-                <div className="w-6 h-1 bg-white"></div>
-
-                <ul className="bg-white w-screen pb-10 absolute -top-full group-focus:-top-1 right-0 duration-150 flex flex-col space-y-3 justify-end items-center shadow-lg z-10">
-                    <button className="px-10 py-8 relative ml-auto">
-                        <div className="w-6 h-1 rotate-45 absolute bg-black"></div>
-                        <div className="w-6 h-1 -rotate-45 absolute bg-black"></div>
-                    </button>
-                    <li className="text-black text-lg font-bold">{username}</li>
-                    <SignoutButton />
-                    <Delete/>
-                </ul>
-            </button>
+            
         </div>
     )
 }
